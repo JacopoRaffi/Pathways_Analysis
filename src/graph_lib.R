f9062dbb83b318f82e295d3c497e3be50c48c12d
JacopoRaffi <75311297+JacopoRaffi@users.noreply.github.com>
2023-05-15 09:08:24 -0400

    completed lib

--- src/graph_lib.R
+++ src/graph_lib.R
@@ -19,22 +19,7 @@ biopax2igraph <- function(file_name){
     V(ig)[i]$name = df_dn[df_dn$id == V(ig)[i]$name]$property_value 
   }
   
-  names = V(ig)$name #vettore dei nomi dei nodi del grafo
-  ids = c(1) #vettore dei futuri id del grafo
-  last_i_used = 1 #conteggio numero sostituzioni
-  
-  for(i in 2:length(names)){
-    if(names[i] %in% names[1:i-1]){ #nome del node già incontrato
-      ids = append(ids, ids[match(names[i], names[1:i-1])]) #inseriamo id del nodo già incontrato
-    }
-    else{
-      last_i_used = last_i_used + 1
-      ids = append(ids, last_i_used) #inseriamo id corretto secondo la progressione
-    }
-  }
-  
-  #fondiamo i nodi con lo stesso nome mantenendo una sola occorrenza per quel nome
-  ig = contract(ig, ids, vertex.attr.comb = list(name="first"))
+  ig = contractNodes(ig)
   
   return(ig)   
 }
@@ -54,7 +39,7 @@ folder2igraph <- function(dir_name){
 
 csv2igraph <- function(file_name, file_dict){
   data = read.csv(file_name)
-  genes = data$GenBank.Gene.ID
+  genes = data$Gene.Name
   prots = data$UniProt.ID
   drugs_ids = data$Drug.IDs
   edges = c()
@@ -63,16 +48,18 @@ csv2igraph <- function(file_name, file_dict){
     drugs = strsplit(drugs_ids, "; ")[[1]]
     uniprot = prots[i]
     
-    if(!(genes[i] == "")){
-      for(j in length(drugs)){
-        edges = append(edges, drugs[j])
-        edges = append(edges, genes[i])
-        #edges = append(edges, c(drugs[j], genes[i]))
-        if (uniprot != ""){
+    if(!(is.na(genes[i]))){
+      if(!(genes[i] == "")){
+        for(j in length(drugs)){
+          edges = append(edges, drugs[j])
           edges = append(edges, genes[i])
-          edges = append(edges, uniprot)
-        } 
+          #edges = append(edges, c(drugs[j], genes[i]))
+          if (uniprot != ""){
+            edges = append(edges, genes[i])
+            edges = append(edges, uniprot)
+          } 
           #edges = append(edges, c(genes[i], uniprot))
+        }
       }
     }
     else{
@@ -100,36 +87,131 @@ splitComplexes <- function(biopax){
     components = c()
     df = splitComplex(biopax, complex_ids[i])
     ids = df$id
-    for(j in 1:length(ids)){
-      if(startsWith(ids[j], "Protein")){
-        prot_df = biopax$dt[biopax$dt$id == ids[j]]
-        prot_ref = sub("#", "", prot_df[prot_df$property == "entityReference"]$property_attr_value)
-        
-        prot_ref_df = biopax$dt[biopax$dt$id == prot_ref]
-        uniprot_gene = prot_ref_df$property_value[startsWith(prot_ref_df$property_value, "UniProt")]
-        
-        uniprot_gene = sub("UniProt:", "", uniprot_gene)
-        parts = strsplit(uniprot_gene, " ")[[1]]
-        
-        uniprotID = parts[1]
-        gene = parts[2]
-        components = append(components, c(gene, uniprotID))
-      }
-      else{
-        components = append(components, df[df$id == ids[j]]$name)
+    
+    if(length(ids) > 0){
+      for(j in 1:length(ids)){
+        if(startsWith(ids[j], "Protein")){
+          prot_df = biopax$dt[biopax$dt$id == ids[j]]
+          prot_ref = sub("#", "", prot_df[prot_df$property == "entityReference"]$property_attr_value)
+          
+          prot_ref_df = biopax$dt[biopax$dt$id == prot_ref]
+          uniprot_gene = prot_ref_df$property_value[startsWith(prot_ref_df$property_value, "UniProt")]
+          
+          if(identical(uniprot_gene,character(0))){
+            next
+          }
+          
+          uniprot_gene = sub("UniProt:", "", uniprot_gene)
+          parts = strsplit(uniprot_gene, " ")[[1]]
+          
+          uniprotID = parts[1]
+          gene = parts[2]
+          components = append(components, c(gene, uniprotID))
+        }
+        else{
+          components = append(components, df[df$id == ids[j],]$name)
+        }
       }
     }
-    
+    if(length(components) == 0){
+      next
+    }
     all_complex_components[[i]] = components
   }
   
   return(all_complex_components)
 }
 
-splitNode <- function(file_name){
+splitNodes <- function(graph, file_name){
   biopax = readBiopax(file_name)
   
-  complexes_components = splitComplexes(biopax)
   complexes = biopax$dt[biopax$dt$class == "Complex"]
+  
+  if(nrow(complexes) == 0){
+    return(graph)
+  }
+  
+  complexes_components = splitComplexes(biopax)
   complex_names = complexes[complexes$property == "displayName"]$property_value 
+  
+  
+  for(i in 1:length(complex_names)){
+    components = complexes_components[[i]]
+    
+    if(!(complex_names[i] %in% V(graph)$name)){
+      next
+    }
+    
+    i_edges = incident(graph, complex_names[i], mode = "in")
+    i_nodes = ends(graph, E(graph)[i_edges])[,1]
+    
+    o_edges = incident(graph, complex_names[i], mode = "out")
+    o_nodes = ends(graph, E(graph)[o_edges])[,2]
+    
+    if(length(components) > 0){
+      for(j in 1:length(components)){
+        new_edges = c()
+        
+        graph = add_vertices(graph, 1, name = components[j]) 
+        
+        if(length(i_nodes) > 0){
+          for(k in 1:length(i_nodes)){
+            
+            new_edges = append(new_edges, c(i_nodes[k], components[j]))
+          }
+          
+          graph = add_edges(graph, new_edges)
+        }
+        
+        if(length(o_nodes) > 0){
+          new_edges = c()
+          for(k in 1:length(o_nodes)){
+            
+            new_edges = append(new_edges, c(components[j], o_nodes[k])) 
+          }
+          
+          graph = add_edges(graph, new_edges)
+        }
+      }  
+    }
+    
+    graph = delete_vertices(graph, complex_names[i])
+  }
+  
+  return(graph)
+}
+
+splitFolderNodes <- function(graph, dir_name){
+  files = list.files(dir_name, full.names = TRUE)
+  
+  for(i in 1:length(files)){
+    graph = splitNodes(graph, files[i])
+  }
+  
+  graph = contractNodes(graph)
+  
+  return(graph)
+}
+
+contractNodes <- function(ig){
+  vrtx = V(ig)
+  
+  names = V(ig)$name #vettore dei nomi dei nodi del grafo
+  ids = c(1) #vettore dei futuri id del grafo
+  last_i_used = 1 #conteggio numero sostituzioni
+  
+  for(i in 2:length(names)){
+    if(names[i] %in% names[1:i-1]){ #nome del node già incontrato
+      ids = append(ids, ids[match(names[i], names[1:i-1])]) #inseriamo id del nodo già incontrato
+    }
+    else{
+      last_i_used = last_i_used + 1
+      ids = append(ids, last_i_used) #inseriamo id corretto secondo la progressione
+    }
+  }
+  
+  #fondiamo i nodi con lo stesso nome mantenendo una sola occorrenza per quel nome
+  ig = contract(ig, ids, vertex.attr.comb = list(name="first"))
+  
+  return(ig)
 }
