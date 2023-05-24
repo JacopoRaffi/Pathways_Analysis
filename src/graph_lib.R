e6ab87cf9de9cf88556fabe71eada1e63232da6f
JacopoRaffi <75311297+JacopoRaffi@users.noreply.github.com>
2023-05-24 15:39:24 +0200

    ADD t-test

--- src/graph_lib.R
+++ src/graph_lib.R
@@ -2,31 +2,33 @@ library(rBiopaxParser)
 library(igraph)
 
 biopax2igraph <- function(file_name){
-  #Creazione di un biopax object da trasformare in un igraph
+  #used for converting BioPax3 files in igraph
+  
   bp = readBiopax(file_name)
+  df = bp$dt # complete dataframe extracted from the BioPax3
+  df_dn = df[df$property == "displayName"] # dataframe filtering for mantaining only the rows with "displayName" as property
   
-  df = bp$dt #dataframe of BioPax model (dataframe completo)
-  df_dn = df[df$property == "displayName"] #df con solo i displayName
-  
+  # Pathway1 -> identifier of the pathway to convert from the dataframe 
   g = pathway2Graph(bp, "Pathway1", useIDasNodenames = FALSE, verbose = FALSE, splitComplexMolecules = TRUE)
   
-  ig = graph_from_graphnel(g, name = TRUE) #grafo estratto dal pathway (igraph)
+  ig = graph_from_graphnel(g, name = TRUE) #graph extracted from the pathway (igraph)
   
+  # substitution of vertex names with "displayName" -> uniformation of the data
   vrtx = V(ig)
-  
-  #sostituiamo i nomi dei nodi del grafo con i corrispettivi displayName del pathway 
   for(i in 1:length(vrtx)){
     V(ig)[i]$name = df_dn[df_dn$id == V(ig)[i]$name]$property_value 
   }
   
+  # merging of nodes which are modeling the same component
   ig = contractNodes(ig)
   
   return(ig)   
 }
 
 folder2igraph <- function(dir_name){
-  files = list.files(dir_name, full.names = TRUE) #ottengo la lista di file biopax (path relativi)
+  #used for converting all the BioPax3 in a folder to igraph
   
+  files = list.files(dir_name, full.names = TRUE)
   fullgraph = biopax2igraph(files[1])
   
   for(i in 2:length(files)){
@@ -38,6 +40,8 @@ folder2igraph <- function(dir_name){
 }
 
 csv2igraph <- function(file_name, file_dict){
+  #used for converting CSVs of drugs'interactions in igraph format
+  
   data = read.csv(file_name)
   genes = data$Gene.Name
   prots = data$UniProt.ID
@@ -54,24 +58,15 @@ csv2igraph <- function(file_name, file_dict){
         for(j in 1:length(drugs)){
           edges = append(edges, drugs[j])
           edges = append(edges, genes[i])
-          #edges = append(edges, c(drugs[j], genes[i]))
-          #if (uniprot != ""){
-          #  edges = append(edges, genes[i])
-          #  edges = append(edges, uniprot)
-          #} 
-          #edges = append(edges, c(genes[i], uniprot))
         }
       }
     }
-    #else{
-      for(j in 1:length(drugs)){
-        if (uniprot != ""){
-          edges = append(edges, drugs[j])
-          edges = append(edges, uniprot)
-        } 
-          #edges = append(edges, c(drugs[j], uniprot))
+    for(j in 1:length(drugs)){
+      if (uniprot != ""){
+        edges = append(edges, drugs[j])
+        edges = append(edges, uniprot)
       }
-    #}
+    }
   }
   ig = make_graph(edges)
   
@@ -79,6 +74,9 @@ csv2igraph <- function(file_name, file_dict){
 }
 
 splitComplexes <- function(biopax){
+  # used for identifying complexes and their components
+  # The information retrieved from this processing are used to split the actual nodes in the already built graph
+  
   complexes = biopax$dt[biopax$dt$class == "Complex"]
   complex_ids = unique(complexes$id)
   
@@ -124,14 +122,16 @@ splitComplexes <- function(biopax){
 }
 
 splitNodes <- function(graph, file_name){
-  biopax = readBiopax(file_name)
+  # used to split graph's nodes which are modeling complexes
   
+  biopax = readBiopax(file_name)
   complexes = biopax$dt[biopax$dt$class == "Complex"]
   
   if(nrow(complexes) == 0){
     return(graph)
   }
   
+  # complexes_components contains the information useful to split the nodes
   complexes_components = splitComplexes(biopax)
   complex_names = complexes[complexes$property == "displayName"]$property_value 
   
@@ -149,6 +149,7 @@ splitNodes <- function(graph, file_name){
     o_edges = incident(graph, complex_names[i], mode = "out")
     o_nodes = ends(graph, E(graph)[o_edges])[,2]
     
+    # thanks to this iteration each component of a complex models all the interactions of the complex
     if(length(components) > 0){
       for(j in 1:length(components)){
         new_edges = c()
@@ -183,6 +184,7 @@ splitNodes <- function(graph, file_name){
 }
 
 splitFolderNodes <- function(graph, dir_name){
+  # used to split complexes of every pathway found in a folder
   files = list.files(dir_name, full.names = TRUE)
   
   for(i in 1:length(files)){
@@ -195,6 +197,7 @@ splitFolderNodes <- function(graph, dir_name){
 }
 
 interaction2igraph <- function(file_name){
+  # used for translating BioGrid interactions in actual edges of the graph
   df = read.csv(file_name)
   
   int_a = df$official_symbol_for_interactor_a
@@ -203,7 +206,6 @@ interaction2igraph <- function(file_name){
   edges = c()
   
   for(i in 1:length(int_a)){
-    print(paste0("INT1: ", i))
     edges = append(edges, c(int_a[i], int_b[i]))
   }
   
@@ -213,6 +215,9 @@ interaction2igraph <- function(file_name){
 }
 
 interaction2igraph2 <- function(file_name, gene_names){
+  # used for translating BioGrid interactions in actual edges of the graph
+  # the framework contains two version useful for the different formatting of those files
+  
   df = read.csv(file_name, sep = "\t", header = TRUE)
   
   int_a = df$Official.Symbol.Interactor.A
@@ -233,43 +238,56 @@ interaction2igraph2 <- function(file_name, gene_names){
 }
 
 contractNodes <- function(ig){
+  # used to merge the nodes which are modeling the same "biological entity"
+  
   vrtx = V(ig)
   
-  names = V(ig)$name #vettore dei nomi dei nodi del grafo
-  ids = c(1) #vettore dei futuri id del grafo
-  last_i_used = 1 #conteggio numero sostituzioni
+  names = V(ig)$name # nodes'names
+  ids = c(1) # vector of IDs necessary for igraph.contract
+  last_i_used = 1 # count of substitutions
   
   for(i in 2:length(names)){
-    if(names[i] %in% names[1:i-1]){ #nome del node già incontrato
-      ids = append(ids, ids[match(names[i], names[1:i-1])]) #inseriamo id del nodo già incontrato
+    if(names[i] %in% names[1:i-1]){ # if the node has already been found (same name)
+      ids = append(ids, ids[match(names[i], names[1:i-1])]) # insert the IDs referring to the already encountered node
     }
     else{
       last_i_used = last_i_used + 1
-      ids = append(ids, last_i_used) #inseriamo id corretto secondo la progressione
+      ids = append(ids, last_i_used) # elsewise insert the ID according to the progression
     }
   }
   
-  #fondiamo i nodi con lo stesso nome mantenendo una sola occorrenza per quel nome
+  # this call result in the marging of all the nodes with the same displayName, preserving only one cumulative instance
   ig = contract(ig, ids, vertex.attr.comb = list(name="first"))
   
   return(ig)
 }
 
 ranking <- function(ig, drugs, genes, directed){
+  
+  # ig -> igraph representation of the global network
+  # drugs -> vector containing all drugs
+  # genes -> vector containing all the genes of the studied disease
+  # directed -> boolean: TRUE: consider edges directions, FALSE: elsewise
+  
   m = "all"
   if(directed)
     m = "out"
   
+  # igraph library function that calculate the shortest path lenghts between two sets of nodes
   dist = distances(ig, v = drugs, to = genes, mode = m)
-  #columns: 1 -> drug_id ; 2 -> mean_distance; 3 -> rank; 4 -> percentage of drugs falling under this rank
-  means = matrix(nrow = length(drugs), ncol = 4)
   
+  # generation of a matrix containing association between drugs and mean distances from the genes of the disease
+  # columns: 1 -> drug_id ; 2 -> mean_distance; 3 -> rank; 4 -> percentage of drugs falling under this rank
+  means = matrix(nrow = length(drugs), ncol = 4)
   for(i in 1:nrow(dist)){
     means[i,1] = drugs[i]
     means[i,2] = mean(dist[i,])
   }
   
+  # sorting of the matrix based on the mean distances: -> the drugs column represent the ranking
   means = means[order(as.numeric(means[,2]), decreasing = FALSE),]
+
+  # populating the rank column
   rank = 1
   last_mean = means[1,2]
   rows = nrow(means)
@@ -282,16 +300,98 @@ ranking <- function(ig, drugs, genes, directed){
     means[i,3] = rank
   }
   
+  # populating the "percentage of drugs falling under this rank" column
   rank_percentage = c()
   for(i in 1:rank){
-    rank_percentage = append(rank_percentage, round((nrow(means[as.numeric(means[,3]) > i,]) / rows) * 100, 2))
+    curr_rank_rows = nrow(means[as.numeric(means[,3]) == i,])
+    lower_rank_rows = nrow(means[as.numeric(means[,3]) > i,])
+    rank_percentage = append(rank_percentage, round( ( (lower_rank_rows + (curr_rank_rows/2)) / rows) * 100, 2))
   }
-  print(rank_percentage)
+  
+  if(genes[1] == "PAH" | genes[1] == "HPD" | genes[1] == "SLC7A7")
+    rank_percentage = append(rank_percentage, round((2219 / rows) * 100, 2), after = 0)
+
   for(i in 1:rows){
     means[i,4] = rank_percentage[as.numeric(means[i,3])]
   }
   
   return (means);
+}
+
+test1Ranking <- function(akdv, arv){
+  # after performing ttest between rank1 and rank2, if pval is reasonably low ( < 0,05), the test assess that the
+  # already known drugs achieve a better ranking on their target diseases respect that on NON target diseases
+  
+  rank1 = c() # ranks achieved by known drugs in each of their target diseases
+  rank2 = c() # ranks achieved by known drugs in each of their NON target diseases
+  
+  for(i in 1:length(akdv)){
+    drugID = akdv[[i]][[1]]
+    
+    for(j in 1:length(arv)){
+      tmp = arv[[j]]
+      maxRank = as.numeric(tmp[nrow(tmp), 3])
+      
+      if(j %in% akdv[[i]]){ # drug i is known for disease j
+        rank1 = append(rank1, as.numeric(tmp[tmp[,1] == drugID, 3]) / maxRank)
+      }
+      else{ 
+        rank2 = append(rank2, as.numeric(tmp[tmp[,1] == drugID, 3]) / maxRank)
+      }
+    }
+  }
+  
+  test = t.test(rank1, rank2)
+  return (test)
+}
+
+test2Ranking <- function(akdv, arv){
+  # after performing ttest between rank1 and rank3, if pval is reasonably low ( < 0,05), the test assess that the
+  # already known drugs achieve a better ranking respect that random drugs
+  
+  rank1 = c() # ranks achieved by known drugs in each of their target diseases
+  rank3 = c() # 10 iterations of the rankings for each disease of 14 random drugs (from the total 2220 drugs list)
+  
+  for(i in 1:length(akdv)){
+    drugID = akdv[[i]][[1]]
+    
+    for(j in 1:length(arv)){
+      tmp = arv[[j]]
+      maxRank = as.numeric(tmp[nrow(tmp), 3])
+      
+      if(j %in% akdv[[i]]){ # drug i is known for disease j
+        rank1 = append(rank1, as.numeric(tmp[tmp[,1] == drugID, 3]) / maxRank)
+      }
+    }
+  }
+  
+  for(i in 1:10){
+    randomDrugs = c()
+    alreadyTaken = c()
+    j = 1
+    while (j <= 14){
+      tmp = round(runif(1, 1, 2220))
+      if(! (tmp %in% alreadyTaken)){
+        randomDrugs = append(randomDrugs, arv[[1]][tmp, 1]) 
+        j = j + 1
+      }
+    }
+    
+    for(k in 1:length(randomDrugs)){
+      drugID = randomDrugs[k]
+      
+      for(m in 1:length(arv)){
+        tmp = arv[[m]]
+        maxRank = as.numeric(tmp[nrow(tmp), 3])
+        rank3 = append(rank3, as.numeric(tmp[tmp[,1] == drugID, 3]) / maxRank)
+      }
+    }
+  }
+  
+  print(rank3)
+  
+  test = t.test(rank1, rank3)
+  return (test)
 }
   
 
--- src/pathway_analysis.R
+++ src/pathway_analysis.R
@@ -2,23 +2,34 @@ source("graph_lib.R")
 library(biogridr)
 library(dplyr)
 
-bg_get_key("Jacopo", "Raffi", "j.raffi@studenti.unipi.it", "metabolic_disease")
+# *** Download and Integration of diseases' and drugs' pathways ***
 
+#Generation of graphs starting form Reactome's Pathways
+ig_diseases = folder2igraph("../data")
+#Data Processing: splitting of complex nodes in components
+ig_diseases = splitFolderNodes(ig_diseases, "../data")
+#Dataframe containing all the pharmacologically active drugs retrieved from DrugBank
 drugs = read.csv("../drugs/pharmacologically_active.csv")
-
-#bgrd_data = read.csv("../BIOGRID-ORGANISM-Homo_sapiens-4.4.221.tab3.txt", sep = "\t", header = TRUE)
-
+ig_drugs = csv2igraph("../drugs/pharmacologically_active.csv", "../drugs/drugbank vocabulary.csv")
+#Extraction of drugs'tragets from DrugBank
 gene_names = drugs$Gene.Name
+#Filtering
 gene_names = gene_names[gene_names != "" & !is.na(gene_names)]
+#Redundant Integration of the genes identifying the studied diseases
 gene_names = append(gene_names, c("PAH", "FAH", "HPD", "TAT", "CBS", "MMADHC", "MTHFR", "MTR", "MTRR", "AMT", "GLDC", "OTC", "SLC3A1", "SLC7A9", "SLC7A7", "PCCA", "PCCB", "MCEE", "MMAA", "MMAB", "MMADHC", "MMUT","BCKDHA", "BCKDHB", "DBT", "PPM1K"))
+#Filtering
 gene_names = unique(gene_names)
+
+
+# *** Query to Biogrid in order to extract the (distance +1) interactions of our pathways'genes ***
+
+#Key for accessing Biogrid from R
+bg_get_key("Jacopo", "Raffi", "j.raffi@studenti.unipi.it", "metabolic_disease")
 
 biogrid_df = as.data.frame(bg("interactions") %>% 
                               bg_constrain(geneList = gene_names[1]) %>% 
                               bg_get_results())
-
 biogrid_df = as.data.frame(biogrid_df[gene_names[1] %in% biogrid_df$official_symbol_for_interactor_a || gene_names[1] %in% biogrid_df$official_symbol_for_interactor_b])
-
 for(i in 2:length(gene_names)){
   print(paste0("BIOGRID query: ", i))
   tmp_df = as.data.frame(bg("interactions") %>% 
@@ -29,23 +40,23 @@ for(i in 2:length(gene_names)){
   
   biogrid_df = rbind(biogrid_df, tmp_df)
 }
-
 write.csv(biogrid_df, "../biogrid.csv")
 
+#Generation of the graph starting from BioGrid's Interaction obtained by query 
 int_ig = interaction2igraph("../biogrid.csv")
+#Generation of the graph starting from BioGrid's Interaction for human organism
 int_ig2 = interaction2igraph2("../BIOGRID-ORGANISM-Homo_sapiens-4.4.221.tab3.txt", gene_names)
 
-ig_drugs = csv2igraph("../drugs/pharmacologically_active.csv", "../drugs/drugbank vocabulary.csv")
 
-ig_diseases = folder2igraph("../data")
-ig_diseases = splitFolderNodes(ig_diseases, "../data")
-
+# *** Generation of the complete graph ***
 full_ig = ig_diseases %u% ig_drugs %u% int_ig %u% int_ig2
 
 
 #Diseases: Name - Gene1 - Gene2 - ... - GeneN
 phenylketonuria = c("Phenylketonuria", "PAH")
-tyrosinemia = c("Tyrosinemia", "FAH", "HPD", "TAT")
+tyrosinemia1 = c("Tyrosinemia T1", "FAH")
+tyrosinemia2 = c("Tyrosinemia T2", "TAT")
+tyrosinemia3 = c("Tyrosinemia T3", "HPD")
 homocystinuria = c("HomoCystinuria", "CBS", "MMADHC", "MTHFR", "MTR", "MTRR")
 hyperglycinemia = c("Hyperglycinemia", "AMT", "GLDC")
 otc_deficiency = c("OTC Deficiency", "OTC")
@@ -55,7 +66,7 @@ propionic_acidemia = c("Propionic Acidemia", "PCCA", "PCCB")
 methylmalonic_acidemia = c("Methylmalonic Acidemia", "MCEE", "MMAA", "MMAB", "MMADHC", "MMUT")
 maple_syrup_disease = c("Maple Syrup Disease", "BCKDHA", "BCKDHB", "DBT", "PPM1K")
 
-#All Drugs Vector
+#Data Processing: unique vector  for all drugs'IDs
 tmp = drugs$Drug.IDs
 adv = c()
 for(i in 1:length(tmp)){
@@ -63,9 +74,12 @@ for(i in 1:length(tmp)){
 }
 adv = unique(adv)
 
+full_ig = read_graph("../graph/full_ig.graphml", format = "graphml")
 #Building Ranking Matrix - Undirected Graphs
 rnks_und_phenylketonuria = ranking(full_ig, adv, phenylketonuria[2:length(phenylketonuria)], FALSE)
-rnks_und_tyrosinemia = ranking(full_ig, adv, tyrosinemia[2:length(tyrosinemia)], FALSE)
+rnks_und_tyrosinemia1 = ranking(full_ig, adv, tyrosinemia1[2:length(tyrosinemia1)], FALSE)
+rnks_und_tyrosinemia2 = ranking(full_ig, adv, tyrosinemia2[2:length(tyrosinemia2)], FALSE)
+rnks_und_tyrosinemia3 = ranking(full_ig, adv, tyrosinemia3[2:length(tyrosinemia3)], FALSE)
 rnks_und_homocystinuria = ranking(full_ig, adv, homocystinuria[2:length(homocystinuria)], FALSE)
 rnks_und_hyperglycinemia = ranking(full_ig, adv, hyperglycinemia[2:length(hyperglycinemia)], FALSE)
 rnks_und_otc_deficiency = ranking(full_ig, adv, otc_deficiency[2:length(otc_deficiency)], FALSE)
@@ -77,7 +91,9 @@ rnks_und_maple_syrup_disease = ranking(full_ig, adv, maple_syrup_disease[2:lengt
 
 #Building Ranking Matrix - Directed Graphs
 rnks_d_phenylketonuria = ranking(full_ig, adv, phenylketonuria[2:length(phenylketonuria)], TRUE)
-rnks_d_tyrosinemia = ranking(full_ig, adv, tyrosinemia[2:length(tyrosinemia)], TRUE)
+rnks_d_tyrosinemia1 = ranking(full_ig, adv, tyrosinemia1[2:length(tyrosinemia1)], TRUE)
+rnks_d_tyrosinemia2 = ranking(full_ig, adv, tyrosinemia2[2:length(tyrosinemia2)], TRUE)
+rnks_d_tyrosinemia3 = ranking(full_ig, adv, tyrosinemia3[2:length(tyrosinemia3)], TRUE)
 rnks_d_homocystinuria = ranking(full_ig, adv, homocystinuria[2:length(homocystinuria)], TRUE)
 rnks_d_hyperglycinemia = ranking(full_ig, adv, hyperglycinemia[2:length(hyperglycinemia)], TRUE)
 rnks_d_otc_deficiency = ranking(full_ig, adv, otc_deficiency[2:length(otc_deficiency)], TRUE)
@@ -87,46 +103,68 @@ rnks_d_propionic_acidemia = ranking(full_ig, adv, propionic_acidemia[2:length(pr
 rnks_d_methylmalonic_acidemia = ranking(full_ig, adv, methylmalonic_acidemia[2:length(methylmalonic_acidemia)], TRUE)
 rnks_d_maple_syrup_disease = ranking(full_ig, adv, maple_syrup_disease[2:length(maple_syrup_disease)], TRUE)
 
-DB00147
-
-hist(as.numeric(rnks_d_phenylketonuria[,2]), col = "green4", breaks = 40, xlab = "Mean distance from disease components", main = "Tyrosinemia", ylim = c(0,1200))
-hist(as.numeric(rnks_und_phenylketonuria[,2]) + 0.2, col = "lawngreen", add = TRUE, position = "right", breaks = 40)
 
 
-hist(as.numeric(rnks_d_tyrosinemia[,2]), col = "green4", breaks = 50, xlab = "Mean distance from disease components", main = "Tyrosinemia", ylim = c(0,700))
-hist(as.numeric(rnks_und_tyrosinemia[,2])+0.1, col = "lawngreen", add = TRUE, position = "right", breaks = 60)
+hist(as.numeric(rnks_und_phenylketonuria[,2]), col = "orchid", breaks = 20, xlab = "Mean distance from disease components", main = "Phenylketonuria", ylim = c(0,1200))
+
+hist(as.numeric(rnks_und_tyrosinemia1[,2]), col = "orchid", breaks = 20, xlab = "Mean distance from disease components", main = "Tyrosinemia Type 1", ylim = c(0,1200))
+
+hist(as.numeric(rnks_und_tyrosinemia2[,2]), col = "orchid", breaks = 20, xlab = "Mean distance from disease components", main = "Tyrosinemia Type 2", ylim = c(0,1200))
+
+hist(as.numeric(rnks_und_tyrosinemia3[,2]), col = "orchid", breaks = 20, xlab = "Mean distance from disease components", main = "Tyrosinemia Type 3", ylim = c(0,1200))
+
+hist(as.numeric(rnks_und_homocystinuria[,2]), col = "orchid", breaks = 20, xlab = "Mean distance from disease components", main = "Homocystinuria", ylim = c(0,800))
+
+hist(as.numeric(rnks_und_hyperglycinemia[,2]), col = "orchid", breaks = 20, xlab = "Mean distance from disease components", main = "Hyperglycinemia", ylim = c(0,800))
+
+hist(as.numeric(rnks_und_otc_deficiency[,2]), col = "orchid", breaks = 20, xlab = "Mean distance from disease components", main = "OTC Deficiency", ylim = c(0,1200))
+
+hist(as.numeric(rnks_und_cystinuria[,2]), col = "orchid", breaks = 20, xlab = "Mean distance from disease components", main = "Cystinuria", ylim = c(0,800))
+
+hist(as.numeric(rnks_und_lysinuric_protein_intolerance[,2]), col = "orchid", breaks = 20, xlab = "Mean distance from disease components", main = "Lysinuric Protein Intolerance", ylim = c(0,1200))
+
+hist(as.numeric(rnks_und_propionic_acidemia[,2]), col = "orchid", breaks = 20, xlab = "Mean distance from disease components", main = "Propionic Acidemia", ylim = c(0,1500))
+
+hist(as.numeric(rnks_und_methylmalonic_acidemia[,2]), col = "orchid", breaks = 20, xlab = "Mean distance from disease components", main = "Methylmalonic Acidemia", ylim = c(0,1000))
+
+hist(as.numeric(rnks_und_maple_syrup_disease[,2]), col = "orchid", breaks = 20, xlab = "Mean distance from disease components", main = "Maple Syrup Disease", ylim = c(0,800))
 
 
-hist(as.numeric(rnks_d_homocystinuria[,2]), col = "green4", breaks = 90, xlab = "Mean distance from disease components", main = "Homocystinuria", ylim = c(0,600))
-hist(as.numeric(rnks_und_homocystinuria[,2]) + 0.05, col = "lawngreen", add = TRUE, position = "right", breaks = 100)
+# *** STATISTICAL VALIDATION *** #
 
+#Vector containing all rankings
+arv = list(rnks_und_phenylketonuria, rnks_und_tyrosinemia1, rnks_und_tyrosinemia2, rnks_und_tyrosinemia3, rnks_und_homocystinuria, 
+        rnks_und_hyperglycinemia, rnks_und_otc_deficiency, rnks_und_cystinuria, rnks_und_lysinuric_protein_intolerance, 
+        rnks_und_propionic_acidemia, rnks_und_methylmalonic_acidemia, rnks_und_maple_syrup_disease)
 
-hist(as.numeric(rnks_d_hyperglycinemia[,2]), col = "green4", breaks = 50, xlab = "Mean distance from disease components", main = "Hyperglycinemia", ylim = c(0,700))
-hist(as.numeric(rnks_und_hyperglycinemia[,2])+0.1, col = "lawngreen", add = TRUE, position = "right", breaks = 60)
+#Drugs: DrugID - Disease1 - Disease2 - ... - DiseaseN
+d1 = list("DB00131", 1, 3, 4, 7)
+d2 = list("DB01373", 1, 2, 3, 4, 7)
+d3 = list ("DB00360", 1, 2)
+d4 = list ("DB00368", 2)
+d5 = list ("DB00668", 2)
+d6 = list ("DB00988", 2)
+d7 = list ("DB01235", 2)
+d8 = list ("DB00115", 5)
+d9 = list ("DB00200", 5)
+d10 = list ("DB03614", 5)
+d11 = list ("DB03904", 7, 8)
+d12 = list ("DB13146", 9)
+d13 = list ("DB00147", 10, 12)
+d14 = list ("DB00348", 3)
 
-hist(as.numeric(rnks_d_cystinuria[,2]), col = "green4", breaks = 60, xlab = "Mean distance from disease components", main = "Cystinuria", ylim = c(0,700))
-hist(as.numeric(rnks_und_cystinuria[,2])+0.1, col = "lawngreen", add = TRUE, position = "right", breaks = 60)
+#Vector containing all known drugs for analysed diseases
+akdv = list(d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11, d12, d13, d14)
 
-hist(as.numeric(rnks_d_lysinuric_protein_intolerance[,2]), col = "green4", breaks = 60, xlab = "Mean distance from disease components", main = "Lysinuric Protein Intolerance", ylim = c(0,1200))
-hist(as.numeric(rnks_und_lysinuric_protein_intolerance[,2])+0.1, col = "lawngreen", add = TRUE, position = "right", breaks = 60)
+# TESTS #
+# - test description is found commented inside the test function (./graph_lib.R)
 
-hist(as.numeric(rnks_d_propionic_acidemia[,2]), col = "green4", breaks = 60, xlab = "Mean distance from disease components", main = "Propionic Acidemia", ylim = c(0,1500))
-hist(as.numeric(rnks_und_propionic_acidemia[,2])+0.1, col = "lawngreen", add = TRUE, position = "right", breaks = 60)
+test1 = test1Ranking(akdv, arv)
+if(test1$p.value < 0.05) print("TEST 1 PASSED!")
 
-hist(as.numeric(rnks_und_methylmalonic_acidemia[,2]), col = "lawngreen", breaks = 60, xlab = "Mean distance from disease components", main = "Methylmalonic Acidemia", ylim = c(0,400))
-#hist(as.numeric(rnks_und_methylmalonic_acidemia[,2])+0.1, col = "lawngreen", add = TRUE, position = "right", breaks = 60)
-
-hist(as.numeric(rnks_d_maple_syrup_disease[,2]), col = "green4", breaks = 100, xlab = "Mean distance from disease components", main = "Maple Syrup Disease", ylim = c(0,750))
-hist(as.numeric(rnks_und_maple_syrup_disease[,2])-0.2, col = "lawngreen", add = TRUE, position = "right", breaks = 100)
-
-hist(as.numeric(rnks_und_otc_deficiency[,2]), col = "lawngreen", breaks = 30, xlab = "Mean distance from disease components", main = "OTC Deficiency", ylim = c(0,1450), xlim = c(2,9))
-hist(as.numeric(rnks_d_otc_deficiency[,2])+0.01, col = "green4", add = TRUE, position = "right", breaks = 50)
-
-
-
-
-
-
+test2 = test2Ranking(akdv, arv)
+print(test2$p.value)
+if(test2$p.value < 0.05) print("TEST 2 PASSED!")
 
 
 
